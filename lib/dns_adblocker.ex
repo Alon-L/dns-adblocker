95c72bc711c3205b2e739cdee66c941cec598fe6
alon-l <2005alonlivne@gmail.com>
2024-04-12 19:36:36 +0300

    initial commit

---
+++ lib/dns_adblocker.ex
@@ -0,0 +1,18 @@
+defmodule DnsAdblocker do
+  @moduledoc """
+  Documentation for `DnsAdblocker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DnsAdblocker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dns_adblocker_test.exs
@@ -0,0 +1,8 @@
+defmodule DnsAdblockerTest do
+  use ExUnit.Case
+  doctest DnsAdblocker
+
+  test "greets the world" do
+    assert DnsAdblocker.hello() == :world
+  end
+end
